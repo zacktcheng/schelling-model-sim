c099eb7eccbe55cb600a808d8c1c28ffc20e4c7b
Zack Cheng <zack.cheng@westpharma.com>
2025-03-26 02:27:40 +0800

    complete algo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import { initNeighbors } from './algorithms.ts';
+import { useState } from 'react';
+import { initCommunity, runSchellingModel } from './algorithms.ts';
 
 function App() {
-  const nn = initNeighbors(2, 10);
-  console.log(nn)
+  // const [community, setCommunity] = useState([]);
+  // const [threshold, setThreshold] = useState(.5);
+  // const [snapshots, setSnapshots] = useState([]);
+  const community = initCommunity(2, 10);
+  const threshold = 1 - 4/8; // threshold == 1−τ
+  const snapshots = runSchellingModel(community,  threshold);
+  console.log(snapshots)
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
--- src/algorithms.ts
+++ src/algorithms.ts
@@ -8,20 +8,86 @@ function shuffleArray(arr: number[]) {
   }
 }
 
-function initNeighbors(typeCount: number, n: number) {
-  const length = typeCount > 0 ? n**2 : 0;
-  const arr = Array.from({ length }, (_, i) => i % typeCount);
+function initCommunity(typeCount: number, n: number, vacancyCount = 12) {
+  const length = typeCount > 0 ? n**2: 0;
+  const arr = Array.from({ length }, (_, i) => i < vacancyCount ? 0 : i % typeCount + 1);
   shuffleArray(arr);
   const matrix: number[][] = [];
-  for (let i = 0; i < n**2; i += n) matrix.push(arr.slice(i, i + n));
+  for (let i = 0; i < n; i++) matrix.push(arr.slice(n * i, n * (i + 1)));
   return matrix;
 }
 
-function getVacancies(matrix: number[][]) {
-  const vacancies = [];
+function getVacancies(community: number[][]) {
+  const vacancies: number[][] = [];
+  for (let row = 0; row < community.length; row++) {
+    for (let col = 0; col < community[row].length; col++) {
+      if (community[row][col] === 0) {
+        vacancies.push([row, col]);
+      }
+    }
+  }
+  return vacancies;
+}
+
+function relocate(row: number, col: number, vacancies: number[][], community: number[][]) {
+  const vacancy = vacancies[Math.floor(Math.random() * vacancies.length)];
+  community[vacancy[0]][vacancy[1]] = community[row][col];
+  community[row][col] = 0;
+  vacancy[0] = row;
+  vacancy[1] = col;
+}
+
+function getNeigbors(row: number, col: number, community: number[][]) {
+  const max = community.length - 1, min = 0;
+  const leftCol = col > 0 ? col - 1 : max;
+  const rightCol = col < max ? col + 1 : min;
+  const bottomRow = row > 0 ? row - 1 : max;
+  const topRow = row < max ? row + 1 : min;
   
+  const topLeft = community[topRow][leftCol];
+  const top = community[topRow][col];
+  const topRight = community[topRow][rightCol];
+  const left = community[row][leftCol];
+  const right = community[row][rightCol];
+  const bottomLeft = community[bottomRow][leftCol];
+  const bottom = community[bottomRow][col];
+  const bottomRight = community[bottomRow][rightCol];
+  return [topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight];
+}
+
+function relocateAndCountDiffs(community: number[][], threshold: number) {
+  let diffs = 0;
+  for (let row = 0; row < community.length; row++) {
+    for (let col = 0; col < community[row].length; col++) {
+      const neighbors = getNeigbors(row, col, community);
+      const home = community[row][col];
+      const fellowCount = neighbors.filter(neighbor => neighbor === home).length;
+      const satisfactory = fellowCount / 8;
+      if (satisfactory < threshold) {
+        diffs++;
+        relocate(row, col, getVacancies(community), community);
+      }
+    }
+  }
+  return diffs;
+}
+
+function runSchellingModel(
+  community: number[][],
+  threshold: number
+) {
+  const vacancyCount = community.flat().filter(value => value === 0).length; // equilibrium
+  const snapshots = [JSON.parse(JSON.stringify(community))];
+  let diffs = relocateAndCountDiffs(community, threshold);
+  while (diffs > vacancyCount) {
+    diffs = relocateAndCountDiffs(community, threshold);
+    snapshots.push(JSON.parse(JSON.stringify(community)));
+  }
+  snapshots.push(JSON.parse(JSON.stringify(community)));
+  return snapshots;
 }
 
 export {
-  initNeighbors,
+  initCommunity,
+  runSchellingModel,
 };
